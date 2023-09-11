bddc2bac66f97595d63642a59d8437daee447ab3
BorseHaraldsen <Augustborse@gmail.com>
2023-09-11 15:06:25 +0200

    Import finished code from earlier project

--- src/test/java/HttpClientTest.java
+++ src/test/java/HttpClientTest.java
@@ -1,4 +1,14 @@
+import org.example.HttpClient;
+import org.junit.jupiter.api.Test;
+
 public class HttpClientTest {
 
+    @Test
+    void shouldMakeHttpCallAndGet200Response(){
+        HttpClient client = new HttpClient("httpbin.org", 80, "/html");
 
+        assert client.statusCode == 200;
+        assert client.headers.get("Content-Type") != null;
+        assert client.body != null;
+    }
 }
