70a760c2008e6251deed04220dc0e104dcea419e
BorseHaraldsen <Augustborse@gmail.com>
2023-09-15 10:59:48 +0200

    Made it prettier by dividing code to fitting classes

--- src/main/java/org/example/HttpRequest.java
+++ src/main/java/org/example/HttpRequest.java
@@ -1,4 +1,51 @@
 package org.example;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+
 public class HttpRequest {
+
+    private String requestLine;
+    private Map<String, String> headers;
+
+
+
+
+    public HttpRequest(Socket clientSocket) throws IOException {
+
+        headers = new HashMap<>();
+
+        requestLine = readLine(clientSocket);
+
+        String headerLine;
+        while ( !(headerLine = readLine(clientSocket)).isEmpty()) {
+            String[] headerParts = headerLine.split(" *: *", 2);
+            headers.put(headerParts[0], headerParts[1]);
+        }
+        printThings();
+
+
+    }
+
+
+    private String readLine (Socket socket) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int character;
+
+        while ((character = socket.getInputStream().read()) != '\r') {
+            sb.append((char) character);
+        }
+        character = socket.getInputStream().read();
+
+        return sb.toString();
+
+    }
+
+    private void printThings() {
+        System.out.println(requestLine);
+        headers.entrySet().forEach( System.out::println);
+    }
+
 }
--- src/main/java/org/example/HttpResponse.java
+++ src/main/java/org/example/HttpResponse.java
@@ -1,4 +1,20 @@
 package org.example;
 
+import java.nio.charset.StandardCharsets;
+
 public class HttpResponse {
+
+
+    String getResponse() {
+        String responseLine = "HTTP/1.1 200 OK\r\n";
+        String body = "<HTML><H1>OH HI I AM A SERVER</H1></HTML>";
+        String header1 = "Content-Type:text/html\r\n";
+        String header2 = "Content-Length" + body.getBytes().length + "\r\n";
+        String spacer = "\r\n";
+
+        return responseLine + header1 + header2 + spacer + body;
+    }
+
+
+
 }
