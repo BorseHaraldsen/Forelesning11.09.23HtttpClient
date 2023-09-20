fe69df590c2ff5d0c75da781adcd72777fcc145f
BorseHaraldsen <Augustborse@gmail.com>
2023-09-20 12:14:18 +0200

    New class requesthandler, and updates to other classes accordingly

--- src/main/java/org/example/HttpRequestHandler.java
+++ src/main/java/org/example/HttpRequestHandler.java
@@ -4,6 +4,8 @@ import java.net.Socket;
 
 public class HttpRequestHandler extends Thread {
 
+
+    // BUILT IN .START() METHOD OF ALL THREAD - WILL RUN A SEPARATE THREAD
     private final Socket clientSocket;
 
     public HttpRequestHandler(Socket clientSocket) {
